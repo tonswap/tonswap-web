06a314dfdb48c0a2b33d2d44ef8bcb16e7dcdbf7
doron aviguy <doronaviguy@gmail.com>
2022-06-01 11:18:47 +0300

    remove alert

--- src/services/wallets/adapters/TonhubWalletAdapter.ts
+++ src/services/wallets/adapters/TonhubWalletAdapter.ts
@@ -54,13 +54,12 @@ export class TonhubWalletAdapter implements WalletAdapter<TonhubCreatedSession> 
     }
     if(isMobile){
       const link = `https://tonhub.com/transfer/${request.to}?amount=${request.value}&bin=${request.payload}`;
-      alert(link);
 
       window.location.href = link;
       return;
     }
  
-
+    
     const response = await this.tonhubConnector.requestTransaction({
       seed: session.seed,
       appPublicKey: state.wallet.appPublicKey,
