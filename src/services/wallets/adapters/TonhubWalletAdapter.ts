1d383ca9a34ab1036f2798bc232b885c1028f445
denis6421 <denis@orbs.com>
2022-08-08 16:34:38 +0300

    a

--- src/services/wallets/adapters/TonhubWalletAdapter.ts
+++ src/services/wallets/adapters/TonhubWalletAdapter.ts
@@ -13,7 +13,7 @@ export class TonhubWalletAdapter
 
   createSession(): Promise<TonhubCreatedSession> {
     const { location } = document;
-    const url = `${location.protocol}//${location.host}?y=1`
+    const url = `${location.protocol}//${location.host}/test123`
     
     return this.tonhubConnector.createNewSession({
       name: APP_NAME,
