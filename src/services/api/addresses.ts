3ba702b1269ab4e0e23470b973d945838a88ec02
doron aviguy <doronaviguy@gmail.com>
2022-09-06 10:11:20 +0300

    new kitty pool

--- src/services/api/addresses.ts
+++ src/services/api/addresses.ts
@@ -33,7 +33,7 @@ export type PoolInfoRaw = {
 export const MainNetPoolsRoot: { [key: string]: PoolInfo } = {
     "EQDw8mvCaC1-kZaJK2aFFl5zuHXoXI2UkTCcVTbBSeqk9EYC": {
         name: "KittyCoin for TonSwap tests",
-        ammMinter: "EQBVyErgx7BCboNXOx0CwA9KYuLY4kXMHanURAGvMWeO-VCE",
+        ammMinter: "EQAPym289T58PFRLOztjvxhuuKXHj2PZQLnIqSp01_9SNFll",
         tokenMinter: "EQDw8mvCaC1-kZaJK2aFFl5zuHXoXI2UkTCcVTbBSeqk9EYC",
         displayName: "KIT",
         image: KittyKorn,
