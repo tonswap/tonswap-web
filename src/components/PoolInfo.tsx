7b2ae793fd92ffdd7a30af1c52b10fb60abd29af
antongotech <105588893+antongotech@users.noreply.github.com>
2023-01-19 15:27:41 +0200

    numbers fixed

--- src/components/PoolInfo.tsx
+++ src/components/PoolInfo.tsx
@@ -9,6 +9,13 @@ import BN from 'bn.js'
 import useUsdValue from 'hooks/useUsdValue'
 import { useTokenOperationsStore } from 'store/token-operations/hooks'
 
+const calculateDecimals = (val: string) => {
+  const n = parseFloat(val)
+  if (n > 1) return n.toFixed(2)
+  else if (n < 0.01) return n.toFixed(6)
+  else return n.toFixed(4)
+}
+
 export const PoolInfo = () => {
   const [poolInfo, setPoolInfo] = useState<{totalSupply: BN, jettonWalletAddress: Address, mintable: string, tonReserves: BN, tokenReserves: BN} | null>(null);
   const {usd} = useUsdValue(ton.name, fromDecimals(poolInfo?.tonReserves.muln(2) || 0, ton.decimals))
@@ -26,19 +33,19 @@ export const PoolInfo = () => {
 
   return (
     <Box sx={{maxWidth: 380, margin: 'auto'}}>
-      {poolInfo && <Box sx={{ background: '#ADD8E6', borderRadius: 1.5, padding: '8px 16px', fontSize: '13px!important' }}>
-          <Typography align='center'>Pool info</Typography>
+      {poolInfo && <Box sx={{ background: 'rgba(173,216,230,0.6)', borderRadius: 1.5, padding: '8px 16px' }}>
+          <Typography fontSize='12px' align='center'>Pool info</Typography>
           <Box display='flex' alignItems='center' justifyContent='space-between' sx={{ width: '100%' }}>
-            <Typography>Liquidity (TVL)</Typography>
-            <Typography>${usd}</Typography>
+            <Typography fontSize='12px'>Liquidity (TVL)</Typography>
+            <Typography fontSize='12px'>${calculateDecimals(usd)}</Typography>
           </Box>
           <Box display='flex' alignItems='center' justifyContent='space-between' sx={{ width: '100%' }}>
-            <Typography>TON</Typography>
-            <Typography>{fromDecimals(poolInfo.tonReserves, ton.decimals)}</Typography>
+            <Typography fontSize='12px'>TON</Typography>
+            <Typography fontSize='12px'>{calculateDecimals(fromDecimals(poolInfo.tonReserves, ton.decimals))}</Typography>
           </Box>
           <Box display='flex' alignItems='center' justifyContent='space-between' sx={{ width: '100%' }}>
-            <Typography>{selectedToken?.displayName || "TOKEN"}</Typography>
-            <Typography>{selectedToken && fromDecimals(poolInfo.tokenReserves, selectedToken.decimals)}</Typography>
+            <Typography fontSize='12px'>{selectedToken?.displayName || "TOKEN"}</Typography>
+            <Typography fontSize='12px'>{selectedToken && calculateDecimals(fromDecimals(poolInfo.tokenReserves, selectedToken.decimals))}</Typography>
           </Box>
         </Box>
       }
