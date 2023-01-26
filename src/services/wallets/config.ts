0208c5a2cbdded4d07d8a90a4060a0cfe5dcb8a6
antongotech <105588893+antongotech@users.noreply.github.com>
2023-01-26 11:21:07 +0200

    cleanup

--- src/services/wallets/config.ts
+++ src/services/wallets/config.ts
@@ -1,17 +1,7 @@
 import { Adapter, Adapters } from "./types";
 import TonhubImg from "assets/images/shared/tonhub.png";
-import ChromeExtImg from "assets/images/shared/chrome.svg";
-import TonKeeper from "assets/images/shared/tonkeeper.svg";
 
 let adapters: Adapter[] = [
-    // {
-    //     name: "Tonkeeper",
-    //     type: Adapters.TON_KEEPER,
-    //     icon: TonKeeper,
-    //     mobileCompatible: true,
-    //     description: "A mobile wallet in your pocket",
-    //     disabled: false,
-    // },
     {
         name: "Tonhub",
         type: Adapters.TON_HUB,
@@ -20,13 +10,6 @@ let adapters: Adapter[] = [
         description: "Crypto wallet for Toncoin",
         tonConnect: false,
     },
-    // {
-    //     name: "Google Chrome Plugin",
-    //     type: Adapters.TON_WALLET,
-    //     icon: ChromeExtImg,
-    //     mobileCompatible: false,
-    //     description: "TON Wallet Plugin for Google Chrome",
-    // },
 ];
 
 export { adapters };
