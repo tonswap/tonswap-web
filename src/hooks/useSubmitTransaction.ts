87b01d79f4dc402a5b688dfd0bb6b0a562779c38
antongotech <105588893+antongotech@users.noreply.github.com>
2023-02-02 16:27:59 +0200

    fixed transactions bug

--- src/hooks/useSubmitTransaction.ts
+++ src/hooks/useSubmitTransaction.ts
@@ -1,11 +1,11 @@
 import { useSelectedAdapter, useWalletStore } from 'store/wallet/hooks'
 import { client, waitForSeqno } from 'services/api'
-import { Address } from 'ton'
+import { Address, Cell } from 'ton'
 import { walletService } from 'services/wallets/WalletService'
 import { isMobile } from 'react-device-detect'
 import { requestTonConnectTransaction } from 'services/wallets/adapters/TonConnectAdapter'
 import { useTokenOperationsActions } from 'store/token-operations/hooks'
-import { Dispatch, SetStateAction } from 'react'
+import { onSetTransactionStatusManually } from 'store/token-operations/actions'
 
 export const useSubmitTransaction = () => {
   const selectedAdapter = useSelectedAdapter()
@@ -14,35 +14,35 @@ export const useSubmitTransaction = () => {
     sendTransaction,
     sendTonConnectTransaction,
     setTransactionStatus,
-  } = useTokenOperationsActions();
-  const { address, adapterId, session } = useWalletStore();
+  } = useTokenOperationsActions()
+  const { address, adapterId, session } = useWalletStore()
 
   const submitTransaction = async (getTxRequest: () => any, sendAnalyticsEvent: () => (undefined | void), getBalances: () => Promise<any>) => {
     setTransactionStatus(true)
-    const txRequest = await getTxRequest();
+    const txRequest = await getTxRequest()
 
     const waiter = await waitForSeqno(
       client.openWalletFromAddress({
         source: Address.parse(address!!),
-      })
+      }),
     )
-    if(!selectedAdapter?.tonConnect) {
+    if (!selectedAdapter?.tonConnect) {
       const tx = async () => {
-        let deepLinkUrl = await walletService.requestTransaction(adapterId!!, session, txRequest);
-        if (typeof deepLinkUrl === 'string') {
-          if (isMobile) {
-            window.location.href = deepLinkUrl
-          }
+        let deepLinkUrl = await walletService.requestTransaction(adapterId!!, session, txRequest)
+        if (typeof deepLinkUrl === 'string' && isMobile) {
+          window.location.href = deepLinkUrl
         }
       }
       sendTransaction(tx)
-      await waiter();
+      await waiter()
     } else {
-      if(isMobile) {
+      if (isMobile) {
+        onSetTransactionStatusManually(true)
         window.location.href = `https://app.tonkeeper.com`
       }
       sendTonConnectTransaction(async () => await requestTonConnectTransaction(txRequest))
-      await waiter();
+      await waiter()
+      onSetTransactionStatusManually(false)
     }
 
     sendAnalyticsEvent()
